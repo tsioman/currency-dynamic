24036ac374d9ba86e36382951a87784aca4e093b
Alexander Tsioma <a.tsioma@raketa.travel>
2020-07-18 19:25:19 +0200

    config typescript webpack jest eslint babel

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path")
+module.exports = {
+  entry: "./src/index.ts",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsc", ".json"]
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  }
+}
