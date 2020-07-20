8d5116dc8ea0d7fb81f35a963450e4e957215aad
aleksandr tsioma <tsioma@localhost.localdomain>
2020-07-20 11:38:26 +0200

    configure jest enzyme and adds more examples with tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/internals/jestSettings.js"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
   },
   testMatch: [
-    "<rootDir>/src/__tests__/**/*.test.ts"
+    "<rootDir>/src/__tests__/**/*.test.(ts|tsx)"
   ]
 };
