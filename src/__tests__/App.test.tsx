29780a50edfeed2488eb1e70dee6d9be4ed5a850
Alexander Tsioma <tsioma@localhost.localdomain>
2020-08-19 14:44:41 +0200

    update tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { InitialConfig } from "../data";
 import { App } from "../containers/App";
-import { shallow, render, mount } from "enzyme";
+import { Graph } from "../components/Graph/Graph";
+
+import { mount } from "enzyme";
 import { responseRUB } from "../__tests__/__mocks__/rates";
 import { getCurrency } from "../services/Currency";
 
@@ -13,7 +15,9 @@ global.fetch = jest.fn().mockImplementation(() =>
 
 describe("Integration tests cases", () => {
   beforeEach(() => {
-    const getCurrency = jest.fn().mockImplementation(()=>Promise.resolve(responseRUB))
+    const getCurrency = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(responseRUB));
   });
   it("fetch data from API is ok", async () => {
     expect(await getCurrency("RUB")).toEqual(responseRUB);
@@ -21,5 +25,38 @@ describe("Integration tests cases", () => {
   const appComponent = mount(<App initial={InitialConfig} />);
   it("render App with initial data is ok", () => {
     expect(appComponent.render()).toMatchSnapshot();
+  });
+  it("first redner and initial state test [constructor and initial state test]", () => {
+    expect(appComponent.find(".button--active").text()).toBe("RUB");
+    expect(appComponent.find(Graph).prop("data")).toMatchInlineSnapshot(
+      `Array []`
+    );
+  });
+  it("first redner and initial state test [componentDidMount]", () => {
+    expect(appComponent.find(".button--active").text()).toBe("RUB");
+    expect(appComponent.find(Graph).prop("data")).toMatchInlineSnapshot(
+      `Array []`
+    );
+  });
+  it("click on button currency change current currency [componentDidUpdate]", () => {
+    expect(appComponent.find(Graph).prop("data")).toMatchInlineSnapshot(
+      `Array []`
+    );
+    expect(
+      appComponent.find("button").not(".button--active").simulate("click")
+    );
+    expect(appComponent.find(Graph).prop("data")).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          0,
+          300,
+        ],
+        Array [
+          300,
+          0,
+        ],
+      ]
+    `);
+    expect(appComponent.find(".button--active").text()).toBe("USD");
   });
 });
