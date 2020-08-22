159890a63cb05ef5a8ac1c9bfa2cb86b04af0737
Alexander Tsioma <a.tsioma84@gmail.com>
2020-08-22 19:37:30 +0500

    refactoring: add emotion library and change button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,43 @@
 import React from "react";
-import "./Button.css";
+import styled from "@emotion/styled";
+import {css} from "@emotion/core";
 import { ColorSetType } from "../../types/";
 
-type ButtonType = {
-  color: ColorSetType;
-  textButton: string;
+type ButtonStyleType = {
   isActive?: boolean;
+  color: ColorSetType;
+}
+
+type ButtonType = ButtonStyleType & {
+  textButton: string;
   onClick: () => void;
 };
+
+const BaseButton = css`
+  border-radius: 4px;
+  padding: 7px 15px;
+  margin: 0 5px;
+  background-color: white;
+  color: black;
+  cursor: pointer;
+  border: none;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: box-shadow 0.3s cubic-bezier(.25,.8,.25,1);
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 5px 5px rgba(0,0,0,0.22);
+  }
+`
+const ActiveButton = css`
+  top: 1px;
+  background-color: #f0f0f0;
+  box-shadow: 0 -1px 0px rgba(0,0,0,0.12), 0 1px 1px rgba(0,0,0,0.24);
+`
+const ButtonWrapper = styled.button`
+  ${BaseButton};
+  ${({isActive}: ButtonStyleType)=>(isActive ? ActiveButton : '')}
+  ${({color}: ButtonStyleType)=>`color: ${color}`}
+
+`;
 
 export const Button: React.FC<ButtonType> = ({
   color,
@@ -15,14 +45,14 @@ export const Button: React.FC<ButtonType> = ({
   isActive,
   onClick,
 }) => {
-  const className = isActive ? "button button--active" : "button";
   return (
-    <button
-      className={className}
+    <ButtonWrapper
       onClick={onClick}
-      style={{ color: `${color}` }}
+      color={color}
+      isActive={isActive}
     >
       {textButton}
-    </button>
+      
+    </ButtonWrapper>
   );
 };
