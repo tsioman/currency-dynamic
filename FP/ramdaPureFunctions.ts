50bc2528972e844e0c6fa87716ea45225a0772e0
Alexander Tsioma <a.tsioma@raketa.travel>
2020-09-15 23:25:25 +0200

    add ramda pure functions

--- FP/ramdaPureFunctions.ts
+++ FP/ramdaPureFunctions.ts
@@ -1,17 +1,31 @@
-import {
-  compose,
-  //...
-} from "ramda";
+import * as R from "ramda";
 
 // Задание 1
 export type Team = { name: string; score: number };
 
-export const getTopName = compose(/* ... */);
+export const getTopName = R.compose(
+  R.prop("name"),
+  R.last,
+  R.sortBy((t: Team) => t.score)
+);
 
 // Задание 2
 export type QsObj = Record<string, string | number | boolean | object>;
 
-const createQs = compose(/* ... */);
+const createQs = R.compose(
+  R.concat("?"),
+  R.join("&"),
+  R.map((val: [string]) => R.join("=", val)),
+  R.toPairs
+);
 
 // Задание 3
-const parseQs = compose(/* ... */);
+const parseQs = R.compose(
+  R.reduce((acc: QsObj, el: string) => {
+    const [key, val] = el.split("=");
+    acc[key] = val;
+    return acc;
+  }, {}),
+  R.split("&"),
+  R.replace("?", "")
+);
