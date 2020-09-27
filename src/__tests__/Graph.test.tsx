1aff09c564da27c3d9abde11fa3018e235f803ab
Alexander Tsioma <a.tsioma@raketa.travel>
2020-09-27 21:16:22 +0200

    update tests

--- src/__tests__/Graph.test.tsx
+++ src/__tests__/Graph.test.tsx
@@ -10,6 +10,9 @@ describe("Graph render check", () => {
     const wrapper = shallow(<Graph      
       options={{area: {width: 300, height: 300}, color: "red"}}
       data={stableGraph}
+      speed={1}
+      playState={"stopped"}
+      onAnimationStateChange={jest.fn()}
     />)
     expect(
       wrapper.html()
@@ -19,19 +22,25 @@ describe("Graph render check", () => {
     const wrapper = shallow(<Graph
       options={{area: {width: 300, height: 300}, color: "violet"}}
       data={[[20, 40]]}
+      speed={1}
+      playState={"stopped"}
+      onAnimationStateChange={jest.fn()}
     />)
     expect(
       wrapper.children().find('.graphic').render().attr("d")
-    ).toBe("M 0 0 L 20 40")
+    ).toBe("M 20 40 L 20 40")
   })
   it('Graph with init param data=[[20,40],[30,40]] with offset x = 0, y = 0 has coords="M 0 0 L 20 40 L 30 40"', () => {
     const wrapper = shallow(<Graph      
       options={{area: {width: 300, height: 300}, color: "blue"}}
+      speed={1}
+      playState={"stopped"}
+      onAnimationStateChange={jest.fn()}
       data={[[20, 40], [30, 40]]}
     />)
     expect(
       wrapper.children().find('.graphic').render().attr("d")
-    ).toBe("M 0 0 L 20 40 L 30 40")
+    ).toBe("M 20 40 L 20 40 L 30 40")
   })
 
 });
