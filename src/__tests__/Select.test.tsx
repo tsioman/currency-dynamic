ed07d59f36bfa0b914c0447fb2b39a1f3f86e7ed
Alexander Tsioma <a.tsioma@raketa.travel>
2020-09-27 22:19:11 +0200

    add: new tests

---
+++ src/__tests__/Select.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Select } from "../components/Select/Select";
+import renderer from "react-test-renderer";
+
+describe("Select", () => {
+  const values: { label: string; value: string }[] = [
+    { label: "0.5x", value: "0.5" },
+    { label: "Test text", value: "text" },
+  ];
+  it("renders Select with two options", () => {
+    expect(
+      renderer
+        .create(<Select selected="text" values={values} onChange={jest.fn()} />)
+        .toJSON()
+    ).toMatchSnapshot();
+  });
+});
