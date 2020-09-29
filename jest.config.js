3a1f5457197a27ecd61d189bff514caeb2b8a2bc
Alexander Tsioma <a.tsioma@raketa.travel>
2020-09-29 23:14:33 +0200

    add some test

--- jest.config.js
+++ jest.config.js
@@ -12,5 +12,6 @@ module.exports = {
   moduleNameMapper: {
     // https://jestjs.io/docs/en/webpack#handling-static-assets
     "\\.(css|less)$": "<rootDir>/internals/__mocks__/styleMock.js",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
 };
