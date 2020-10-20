ffd87e7f40c4f7e5dff85cd4760e8389d2764b60
Alexander Tsioma <a.tsioma@raketa.travel>
2020-10-20 06:26:50 +0200

    fix: reducer tests

--- src/containers/Auth/reducer.test.ts
+++ src/containers/Auth/reducer.test.ts
@@ -1,20 +1,20 @@
 import { actions, reducer, CheckState, initialState } from "./reducer";
 
 describe("Login reducer", () => {
-  const username = "ComeToMe";
-  it("Attempt login with empty username", () => {
-    expect(reducer(initialState, actions.login(""))).toEqual({
-      username: "",
-      status: CheckState.initiated,
+  const username = "Test user";
+  it("Is set failed status for wrong username length", () => {
+    expect(reducer(initialState, actions.login("Min"))).toEqual({
+      username: "Min",
+      status: CheckState.failed,
     });
   });
-  it("Correct login action", () => {
+  it("Is correct login action", () => {
     expect(reducer(initialState, actions.login(username))).toEqual({
       username,
       status: CheckState.succeed,
     });
   });
-  it("Logout action", () => {
+  it("Is logout action work correctly", () => {
     expect(
       reducer({ username, status: CheckState.succeed }, actions.logout())
     ).toEqual({
