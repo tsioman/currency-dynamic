f3a0c3df2c1f808af4f7dcd77301c2f5288bc3dd
Alexander Tsioma <a.tsioma84@gmail.com>
2020-11-03 23:13:10 +0100

    for PR review

--- src/modules/Currency/module.ts
+++ src/modules/Currency/module.ts
@@ -1,11 +1,9 @@
 import { ISagaModule } from "redux-dynamic-modules-saga";
 import { reducer } from "./reducer";
-import { currencySaga } from "./saga";
 
 export const getCurrencyModule = (): ISagaModule<typeof reducer> => ({
   id: "currency",
   reducerMap: {
     currency: reducer,
   },
-  sagas: [currencySaga],
 });
